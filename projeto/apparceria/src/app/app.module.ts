5b9bb591921bc097bc881d6afff0615af1f9116f
Ana Leite <silvaaraujoleite@gmail.com>
2019-11-20 09:24:37 -0300

    Conclusão da configuração do banco e parte da criação do front-end

--- projeto/apparceria/src/app/app.module.ts
+++ projeto/apparceria/src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { PrincipalComponent } from './principal/principal.component';
+
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PrincipalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
